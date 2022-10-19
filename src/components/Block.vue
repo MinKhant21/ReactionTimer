3e1066d430616e941f021a69139abe8620eea863
Kaung Khant <kaungkhant@Kaungs-MacBook-Air.local>
2022-10-19 17:22:57 +0630

    ReactionTimer

---
+++ src/components/Block.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="block" v-if="ShowBlock" @click="stopTimer">
+    Click Here {{ delay }}
+  </div>
+</template>
+
+<script>
+export default {
+  props:['delay'],
+  data(){
+    return{
+      ShowBlock:false,
+      score:0,
+      timer:null
+    }
+  },
+  mounted(){
+    setTimeout(()=>{
+      this.ShowBlock = true
+      this.startTimer()
+    },this.delay)
+  },
+  methods:{
+    startTimer()
+    {
+      this.timer = setInterval(() => {
+        this.score +=50;
+      }, 50);
+    },
+    stopTimer()
+    {
+      clearInterval(this.timer)
+      this.$emit('endGame',this.score)
+    }
+  }
+ 
+  
+}
+</script>
+
+<style>
+.block
+{
+    width: 300px;
+    height: 300px;
+    padding-top: 20px;
+    margin: 20px auto;
+    background-color: blanchedalmond;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>Result : {{ score }}</p>
+  
+  <div v-if="score < 250">
+    <p>You are So fast !</p>
+  </div>
+  <div v-else-if="score>250 && score < 400">
+    <p>You are normal</p>
+  </div>
+  <div v-else>
+    <p>You are too slow ! Try Again.</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['score']
+}
+</script>
+
+<style>
+
+</style>
